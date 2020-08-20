915cebd42adc35722c20f042d94ac1c9737c4d54
Melina Alejos <malejos1219@gmail.com>
2020-08-20 14:41:58 -0400

    final sprint code

--- src/main/java/com/tts/stream/controller/WatchListController.java
+++ src/main/java/com/tts/stream/controller/WatchListController.java
@@ -1,14 +1,12 @@
 package com.tts.stream.controller;
 
 import java.util.Optional;
-
 import com.tts.stream.model.Movie;
 import com.tts.stream.model.MovieResponse;
 import com.tts.stream.model.User;
 import com.tts.stream.repository.UserRepository;
 import com.tts.stream.service.MovieService;
 import com.tts.stream.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class WatchListController {
-
     @Autowired
     public UserService userService;
-
     @Autowired
     public UserRepository userRepository;
 
@@ -44,7 +40,7 @@ public class WatchListController {
         User currentUser = user.get();
         // System.out.println(currentUser);
         if (currentUser.getWatchList() == null) {
-            currentUser.setWatchList(package1.getMovieId()+","+package1.getType());
+            currentUser.setWatchList(package1.getMovieId() + "," + package1.getType());
             System.out.println(currentUser.getWatchList());
             userRepository.save(currentUser);
         } else {
@@ -52,17 +48,40 @@ public class WatchListController {
             String[] whatever = watchList.split(" ", -1);
             Boolean alreadyInHere = false;
             for (int i = 0; i < whatever.length; i++) {
-                if (whatever[i].equals(package1.getMovieId()+","+package1.getType())) {
+                if (whatever[i].equals(package1.getMovieId() + "," + package1.getType())) {
                     alreadyInHere = true;
                     System.out.println(alreadyInHere);
                 }
             }
             if (!alreadyInHere) {
-                currentUser.setWatchList(currentUser.getWatchList() + " " + package1.getMovieId()+","+package1.getType());
+                currentUser.setWatchList(
+                        currentUser.getWatchList() + " " + package1.getMovieId() + "," + package1.getType());
                 userRepository.save(currentUser);
             }
         }
         System.out.println(currentUser.getWatchList());
     }
 
+    @PostMapping("/watchlist/delete")
+    public void deleteMovieFromWatchlist(@RequestBody MovieResponse package1) {
+        Optional<User> user = userRepository.findById(Long.parseLong(package1.getUserId()));
+        User currentUser = user.get();
+        String userWatchlist = currentUser.getWatchList();
+        String[] whatever = userWatchlist.split(" ", -1);
+        for (int i = 0; i < whatever.length; i++) {
+            if (whatever[i].equals(package1.getMovieId() + "," + package1.getType())) {
+                // whatever[i].replace(whatever[i], "");
+                whatever[i] = "";
+            }
+        }
+        String update = "";
+        for (int i = 0; i < whatever.length; i++) {
+            if (whatever[i] != "") {
+                update = update + " " + whatever[i];
+            }
+        }
+        currentUser.setWatchList(update);
+        userRepository.save(currentUser);
+        System.out.println(update);
+    }
 }
